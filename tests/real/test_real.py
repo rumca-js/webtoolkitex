660facc9faa59f42e10580265eb4f57a61b2c31e
Rumpel Stilskin <rozbujnik@gmial.com>
2025-12-22 14:25:35 +0100

    Updated real test

--- tests/real/test_real.py
+++ tests/real/test_real.py
@@ -13,22 +13,10 @@ from webtoolkitex import (
 from webtoolkitex.webconfig import WebConfig
 
 
-def print_bar():
-    print("------------------------")
-
-
-def is_true(text, condition):
-    #if condition:
-    #    print(f[{text}] OK")
-    if not condition:
-        print(f"Error: {text}")
-
-
 class TestBaseUrl(unittest.TestCase):
     def run_with_base_url(self, test_url, request=None, crawler_name=None, handler_name=None):
 
         if handler_name or crawler_name:
-            print("ddjjdjd")
             request=PageRequestObject(test_url)
 
         if handler_name:
@@ -42,47 +30,20 @@ class TestBaseUrl(unittest.TestCase):
         response = url.get_response()
         handler = url.get_handler()
 
+        self.assertTrue(handler.get_title())
+        self.assertTrue(handler.get_hash())
+        self.assertTrue(handler.get_body_hash())
 
-        #if response is None:
-        #    print("Missing response!")
-        #    return response, handler
+        self.assertTrue(response is not None)
+        self.assertTrue(response.is_valid())
+        self.assertTrue(response.get_text())
+        self.assertTrue(response.get_hash())
+        self.assertTrue(response.get_body_hash())
 
-        #if response.is_invalid():
-        #    print("Invalid response")
-        #    return response, handler
-
-        #if response.is_valid():
-        #    print("Response is valid")
-
-        #if response.get_text() is None:
-        #    print("No text in response")
-        #    return response, handler
-
-        #if handler.get_title() is None:
-        #    print("No title in url")
-        #    return response, handler
-
-        #if not handler.get_hash():
-        #    print("No hash")
-
-        #if not handler.get_body_hash():
-        #    print("No body hash")
-
-        #print("Response request: {}".format(response.request))
-
-        print("Title: {}".format(handler.get_title()))
-
-        entries_len = len(list(handler.get_entries()))
-        print(f"Entries: {entries_len}")
-
-        streams_len = len(list(handler.get_streams()))
-        print(f"Streams: {streams_len}")
-
-        properties = url.get_social_properties()
-        #print(f"Social properties: {properties}")
+        #properties = url.get_social_properties()
+        ##print(f"Social properties: {properties}")
 
         return response, handler
-
 
     def test_baseurl__vanilla_google(self):
         test_url = "https://www.google.com"
@@ -91,13 +52,12 @@ class TestBaseUrl(unittest.TestCase):
         entries_len = len(list(handler.get_entries()))
         streams_len = len(list(handler.get_streams()))
 
-        is_true("Response is valid", response.is_valid())
-        is_true("Title", handler.get_title() == "Google")
-        is_true("Streams_len", streams_len == 1)
-        is_true("Entries_len", entries_len == 0)
+        self.assertTrue("Response is valid", response.is_valid())
+        self.assertTrue("Title", handler.get_title() == "Google")
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
 
         return response,handler
-
 
     def test_baseurl__youtube_video(self):
         test_url = "https://www.youtube.com/watch?v=9yanqmc01ck"
@@ -106,11 +66,10 @@ class TestBaseUrl(unittest.TestCase):
         entries_len = len(list(handler.get_entries()))
         streams_len = len(list(handler.get_streams()))
 
-        is_true("Response is valid", response.is_valid())
-        is_true("Title", handler.get_title() == "The ULTIMATE 16-Player Gaming Setup is COMPLETE!")
-        is_true("Streams_len", streams_len == 2)
-        is_true("Entries_len", entries_len == 0)
-
+        self.assertTrue("Response is valid", response.is_valid())
+        self.assertTrue("Title", handler.get_title() == "The ULTIMATE 16-Player Gaming Setup is COMPLETE!")
+        self.assertTrue("Streams_len", streams_len == 2)
+        self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__youtube_channel__name(self):
         test_url = "https://www.youtube.com/@LinusTechTips"
@@ -119,66 +78,86 @@ class TestBaseUrl(unittest.TestCase):
         entries_len = len(list(handler.get_entries()))
         streams_len = len(list(handler.get_streams()))
 
-        is_true("Response is valid", response.is_valid())
-        is_true("Title", handler.get_title() == "Linus Tech Tips")
-        is_true("Streams_len", streams_len == 1)
-        is_true("Entries_len", entries_len == 0)
-        is_true("Feeds_len", len(handler.get_feeds()) == 1)
+        self.assertTrue("Response is valid", response.is_valid())
+        self.assertTrue("Title", handler.get_title() == "Linus Tech Tips")
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
+        self.assertTrue("Feeds_len", len(handler.get_feeds()) == 1)
 
         #response, handler = self.run_with_base_url(test_url, handler_name="HttpPageHandler")
 
         #entries_len = len(list(handler.get_entries()))
         #streams_len = len(list(handler.get_streams()))
 
-        #is_true("Response is valid", response.is_valid())
-        #is_true("Title", handler.get_title() == "Linus Tech Tips")
-        #is_true("Streams_len", streams_len == 1)
-        #is_true("Entries_len", entries_len == 0)
+        #self.assertTrue("Response is valid", response.is_valid())
+        #self.assertTrue("Title", handler.get_title() == "Linus Tech Tips")
+        #self.assertTrue("Streams_len", streams_len == 1)
+        #self.assertTrue("Entries_len", entries_len == 0)
 
         #response, handler = self.run_with_base_url(test_url, crawler_name="RequestsCrawler")
 
         #entries_len = len(list(handler.get_entries()))
         #streams_len = len(list(handler.get_streams()))
 
-        #is_true("Response is valid", response.is_valid())
-        #is_true("Title", handler.get_title() == "Linus Tech Tips")
-        #is_true("Streams_len", streams_len == 1)
-        #is_true("Entries_len", entries_len == 0)
+        #self.assertTrue("Response is valid", response.is_valid())
+        #self.assertTrue("Title", handler.get_title() == "Linus Tech Tips")
+        #self.assertTrue("Streams_len", streams_len == 1)
+        #self.assertTrue("Entries_len", entries_len == 0)
 
         #response, handler = self.run_with_base_url(test_url, crawler_name="CurlCffiCrawler")
 
         #entries_len = len(list(handler.get_entries()))
         #streams_len = len(list(handler.get_streams()))
 
-        #is_true("Response is valid", response.is_valid())
-        #is_true("Title", handler.get_title() == "Linus Tech Tips")
-        #is_true("Streams_len", streams_len == 1)
-        #is_true("Entries_len", entries_len == 0)
-
+        #self.assertTrue("Response is valid", response.is_valid())
+        #self.assertTrue("Title", handler.get_title() == "Linus Tech Tips")
+        #self.assertTrue("Streams_len", streams_len == 1)
+        #self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__odysee_channel(self):
         test_url = "https://odysee.com/$/rss/@BrodieRobertson:5"
         response, handler = self.run_with_base_url(test_url)
 
+        entries_len = len(list(handler.get_entries()))
+        streams_len = len(list(handler.get_streams()))
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__odysee_video(self):
         test_url = "https://odysee.com/servo-browser-finally-hits-a-major:24fc604b8d282b226091928dda97eb0099ab2f05"
-        return self.run_with_base_url(test_url)
+        response, handler = self.run_with_base_url(test_url)
 
+        entries_len = len(list(handler.get_entries()))
+        streams_len = len(list(handler.get_streams()))
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__github(self):
         test_url = "https://github.com/rumca-js/crawler-buddy"
-        return self.run_with_base_url(test_url)
+        response, handler = self.run_with_base_url(test_url)
 
+        entries_len = len(list(handler.get_entries()))
+        streams_len = len(list(handler.get_streams()))
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__reddit__channel(self):
         test_url = "https://www.reddit.com/r/wizardposting"
-        return self.run_with_base_url(test_url)
+        response, handler = self.run_with_base_url(test_url)
 
+        entries_len = len(list(handler.get_entries()))
+        streams_len = len(list(handler.get_streams()))
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__reddit__news(self):
         test_url = "https://www.reddit.com/r/wizardposting/comments/1olomjs/screw_human_skeletons_im_gonna_get_more_creative/"
-        return self.run_with_base_url(test_url)
+        response, handler = self.run_with_base_url(test_url)
+
+        entries_len = len(list(handler.get_entries()))
+        streams_len = len(list(handler.get_streams()))
+        self.assertTrue("Streams_len", streams_len == 1)
+        self.assertTrue("Entries_len", entries_len == 0)
 
     def test_baseurl__is_allowed(self):
         test_url = "https://www.youtube.com/watch?v=Vzgimftolys&pp=ygUPbGludXMgdGVjaCB0aXBz"
